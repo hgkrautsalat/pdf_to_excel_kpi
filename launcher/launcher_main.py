7a7363af968eeb0144d65bc343faede299405ebc
hgkrautsalat <wiliamforker16@gmail.com>
2026-02-09 19:11:26 +0100

    V1.0.2 Launcher.py angepasst - Check neues Release, Downlaod, abspeichern neuer ReleaseTag, Start App

--- launcher/launcher_main.py
+++ launcher/launcher_main.py
@@ -1,113 +1,17 @@
-# import requests
-# import subprocess
-# import os
-# import sys
-# import json
-
-# APP_EXE = "app.exe"
-# VERSION_URL = "https://raw.githubusercontent.com/hgkrautsalat/pdf_to_excel_kpi/main/version.json"
-# LOCAL_VERSION = "1.0.0"
-
-# def check_for_update()-> None:
-#     try:
-#         r: requests.Response = requests.get(VERSION_URL, timeout=5)
-#         data:requests.Any = r.json()
-
-#         if data["version"] != LOCAL_VERSION:
-#             download_update(data["exe_url"])
-#     except Exception as e:
-#         print()
-#         print("Kein Internet / Fehler → normal starten")
-#         print()
-#         print('Exception:','\n', e)
-#         print()
-#         print()
-
-# def download_update(url):
-#     r = requests.get(url)
-#     with open(APP_EXE, "wb") as f:
-#         f.write(r.content)
-
-# def start_app():
-#     subprocess.Popen(
-#         [sys.executable, os.path.join("converter", "main.py")],
-#         cwd=os.getcwd()
-#     )
-
-# if __name__ == "__main__":
-#     check_for_update()
-#     start_app()
-###################################################################################################################
-# # launcher/launcher_main.py
-# import requests
-# import subprocess
-# import os
-# import json
-
-# VERSION_URL = "https://raw.githubusercontent.com/DEIN_USER/DEIN_REPO/main/version.json"
-# APP_EXE = "app.exe"
-
-# def start_app():
-#     subprocess.Popen([APP_EXE], cwd=os.getcwd())
-
-# def main():
-#     r = requests.get(VERSION_URL)
-#     data = r.json()
-
-#     if not os.path.exists(APP_EXE):
-#         print("App fehlt – Download...")
-#         download(data["download_url"])
-
-#     start_app()
-
-# def download(url):
-#     r = requests.get(url)
-#     with open(APP_EXE, "wb") as f:
-#         f.write(r.content)
-
-# if __name__ == "__main__":
-#     main()
-
-
 import sys
 import requests
 import subprocess
 import os
 import json
 
-VERSION_URL = "https://raw.githubusercontent.com/hgkrautsalat/pdf_to_excel_kpi/main/version.json"
 
-APP_EXE = "converter.exe"
-LOCAL_VERSION_FILE = "version.json"
+LATEST_RELEASE_URL = "https://api.github.com/repos/hgkrautsalat/pdf_to_excel_kpi/releases/latest"
+DOWNLOAD_URL = "https://github.com/hgkrautsalat/pdf_to_excel_kpi/releases/download/"
 
+APP_EXE = "app.exe"
+LOCAL_VERSION_FILE = "version_local.json"
 
-def parse_version(version:str) -> tuple:
-    version_list = version.split(".")
-    version_map = map(int, version_list)
-    version_tuple = tuple(version_map)
-    return version_tuple
-    # return tuple(map(int, version.split(".")))
-
-def load_local_version()-> str:
-    if not os.path.exists(LOCAL_VERSION_FILE):
-        return "0.0.0"
-
-    with open(LOCAL_VERSION_FILE, "r") as f:
-        return json.load(f)["version"]
-
-
-def save_local_version(version:str) -> None:
-    with open(LOCAL_VERSION_FILE, "w") as f:
-        json.dump({"version": version}, f)
-
-
-def download_app(url:str) -> None:
-    print("⬇️ Lade neues Update herunter...")
-    r: requests.Response = requests.get(url)
-    r.raise_for_status()
-
-    with open(APP_EXE, "wb") as f:
-        f.write(r.content)
+DIR_PATH:str = os.path.dirname(os.path.realpath(__file__)).split("launcher")[0]
 
 
 def start_app():
@@ -128,9 +32,11 @@ def start_app():
         PermissionError: Wenn keine Berechtigung zum Ausführen der Datei besteht
     """
     print("🚀 Starte Anwendung...")
+    app_path = os.path.join(DIR_PATH, APP_EXE)
+
     subprocess.Popen(
-        [APP_EXE],
-        cwd=os.getcwd()
+        [app_path],
+        cwd=DIR_PATH
     )
     # subprocess.Popen(
     #     # [sys.executable, os.path.join(APP_EXE, "main.py")],
@@ -138,46 +44,84 @@ def start_app():
     #     cwd=os.getcwd()
     # )
 
-def check_for_update():
-    print("🔍 Prüfe auf Updates...")
+
+def download_app(url:str, version:str) -> None:
+    print("⬇️ Lade neues Update herunter...")
     try:
-        response: requests.Response = requests.get(VERSION_URL, timeout=5)
-        data:dict = response.json()
+        response: requests.Response = requests.get(url)
+        response.raise_for_status()
 
-        remote_version:str = data["version"]
-        download_url:str = data["exe_url"]
+        with open(os.path.join(DIR_PATH, APP_EXE), "wb") as f:
+            f.write(response.content)
 
-        local_version:str = load_local_version()
-
-        print(f"Lokale Version: {local_version}")
-        print(f"Remote Version: {remote_version}")
-
-        remote_version:tuple = parse_version(remote_version)
-        local_version:tuple  = parse_version(local_version)
-        print(f"Vergleiche Versionen: {remote_version} > {local_version}?")
-
-        if remote_version > local_version:
-            print("🆕 Neues Update verfügbar!")
-            download_app(download_url)
-            save_local_version(remote_version)
-            print("✅ Update erfolgreich installiert")
-        else:
-            print("✅ Keine Updates verfügbar")
-
-        # if not os.path.exists(APP_EXE):
-        #     raise RuntimeError("converter.exe fehlt!")
+        save_local_version(version)  # Speichert die Version basierend auf der URL (z.B. "V_1.0.1")
 
     except Exception as e:
-        print()
-        print("Kein Internet / Fehler → normal starten")
-        print()
-        print('Exception:', '\n', e)
-        print()
-        print()
+        print("❌ Fehler beim Herunterladen der neuen Version:", "\n",e)
+
+
+def parse_version(version:str) -> tuple:
+    version_list = version.split("_")[-1].split(".")
+    version_map = map(int, version_list)
+    version_tuple = tuple(version_map)
+    return version_tuple
+
+
+def save_local_version(version:str) -> None:
+    local_path = os.path.join(DIR_PATH, LOCAL_VERSION_FILE)
+    with open(local_path, "w") as f:
+        json.dump({"version": version}, f)
+
+
+def get_local_version(file_name:str)-> str:
+    local_path:str = os.path.join(DIR_PATH, file_name)
+    if not os.path.exists(local_path):
+        save_local_version("V_0.0.0")
+        return "V_0.0.0"
+
+    with open(local_path, "r") as f:
+        local_version:str = json.load(f)["version"]
+        # print(f"Lokale Version aus Datei geladen: {local_version}")
+        return local_version
+
+
+def get_latest_release_tag(release_info:dict) -> str:
+    try:
+        tag_name:str = release_info["tag_name"]
+        return tag_name
+    except Exception as e:
+        print("Fehler beim Abrufen der neuesten Release-Tag:", e)
+        return "V_0.0.0"
+
+
+def get_latest_release_info(release_url:str) -> dict:
+    try:
+        response:requests.Response = requests.get(release_url, timeout=5)
+        response.raise_for_status()
+        release_info:dict = response.json()
+        return release_info
+    except Exception as e:
+        print("Fehler beim Abrufen der neuesten Release-Informationen:", e)
+        return {}
 
 
 def main():
-    check_for_update()
+    latest_release:dict         = get_latest_release_info(LATEST_RELEASE_URL)
+
+    latest_release_tag_name:str    = get_latest_release_tag(latest_release)
+    local_release_tag_name:str     = get_local_version(LOCAL_VERSION_FILE)
+
+    latest_release_tag:tuple = parse_version(latest_release_tag_name)
+    local_release_tag:tuple  = parse_version(local_release_tag_name)
+
+    download_url:str = DOWNLOAD_URL + latest_release_tag_name + "/" + APP_EXE
+
+    if (latest_release_tag > local_release_tag) or not os.path.exists(os.path.join(DIR_PATH, APP_EXE)):
+        print(os.path.join(DIR_PATH, APP_EXE))
+        print("🆕 Neues Update verfügbar!")
+        download_app(download_url,latest_release_tag_name)
+        print("✅ Update erfolgreich installiert")
+
     start_app()
 
 
